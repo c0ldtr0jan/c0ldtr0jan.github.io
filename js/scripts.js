7677bb8c3ebae492fd40df12f9604cdb55c2a5cb
coldtrojan <mark@randomplace.co.uk>
2014-05-11 21:18:18 +0100

    v1

--- js/scripts.js
+++ js/scripts.js
@@ -21,10 +21,32 @@ $(function(){
 		if ($(this).hasClass("slider-left")) {
 			
 			$(this).parent().find('.slider-inner').animate({"margin-left":"+=100%"});
+			$(this).parent().find('.slider-inner ul.active').removeClass('active').prev().addClass('active');
 			
 		} else {
 			
 			$(this).parent().find('.slider-inner').animate({"margin-left":"-=100%"});
+			$(this).parent().find('.slider-inner ul.active').removeClass('active').next().addClass('active');
+			
+		}
+		
+		if ($(this).parent().find('.slider-inner ul.active').prev().length) {
+			
+			$(this).parent().find('a.slider-left').removeClass("hidden");
+			
+		} else {
+			
+			$(this).parent().find('a.slider-left').addClass("hidden");
+			
+		}
+		
+		if ($(this).parent().find('.slider-inner ul.active').next().length) {
+			
+			$(this).parent().find('a.slider-right').removeClass("hidden");
+			
+		} else {
+			
+			$(this).parent().find('a.slider-right').addClass("hidden");
 			
 		}
 		
