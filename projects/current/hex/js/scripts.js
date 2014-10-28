0c9c11d3c344376bd593b7f6a4c92d1f00302476
coldtrojan <mark@randomplace.co.uk>
2014-10-28 02:50:03 +0000

    Link Functionality
    Added link action

--- projects/current/hex/js/scripts.js
+++ projects/current/hex/js/scripts.js
@@ -2,6 +2,128 @@ var outerMargin = 10;
 var hexGap = 10;
 var boxPadding = 5;
 
+var animationTime = 600;
+var animationType = "easeOutExpo";
+
+function loadPage(hexID) {
+
+	$("#box .hex" + hexID).addClass("selected");
+
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	if (windowHeight > windowWidth) {
+
+		var boxHeight = (parseFloat(windowWidth) - (outerMargin * 2));
+
+	} else {
+
+		var boxHeight = (parseFloat(windowHeight) - (outerMargin * 2));
+
+	}
+
+	var hexHeight = ((boxHeight - (hexGap * 2)) / 3);
+
+	var sideLength = (hexHeight / (Math.sqrt(3)));
+	var triHeight = (2 * sideLength) / 2;
+
+	var triSectHeight = (hexHeight / 2);
+
+	var triWidth = Math.sqrt((sideLength * sideLength) - (triSectHeight * triSectHeight));
+
+	var hexWidth = (sideLength + (triWidth * 2));
+
+	var boxWidth = (sideLength + (hexWidth * 2) + (hexGap * 2)); //(sideLength * 3) + (hexGap * 2);
+
+	var hexOffset = (((hexHeight * 2) - hexWidth) / 2);
+
+	var count = 7;
+
+	var newInt = setInterval(function(){
+
+	if (count >= 1) {
+
+		if (count != hexID) {
+
+			if (count == 7) {
+
+				$("#box .hex" + count).animate({"opacity":"0"}, (animationTime / 4), "easeInOutSine");
+
+			} else {
+
+				$("#box .hex" + count).animate({
+					"opacity":"0",
+					"height":hexHeight + "px", 
+					"width":(hexHeight * 2) + "px",
+					"top":"50%",
+					"margin-top": - (hexHeight / 2) + "px",  
+					"left":"50%", 
+					"margin-left": - hexHeight + "px"
+				}, (animationTime), animationType);
+
+			}
+
+		} else {
+
+			if (count == 7) {
+
+
+			} else {
+
+				$("#box .hex" + count).animate({
+					"height":hexHeight + "px", 
+					"width":(hexHeight * 2) + "px",
+					"top":"50%",
+					"margin-top": - (hexHeight / 2) + "px",  
+					"left":"50%", 
+					"margin-left": - hexHeight + "px"
+				}, (animationTime), animationType);
+
+			}
+
+		}
+
+		count--;
+
+	} else {
+
+		clearInterval(newInt);
+
+		setTimeout(function(){
+
+			$("#box .hex.selected").addClass("animate");
+
+			//replace with loader script
+			setTimeout(function(){
+
+				$("#box .hex.selected").one('animationiteration webkitAnimationIteration', function() {
+			        $(this).removeClass("animate");
+
+		        	$("#box").css({
+		        	
+		        		"height": hexHeight + "px",
+						"margin-top": - (hexHeight / 2) + "px",
+						"width": hexWidth + "px",
+						"margin-left": -((hexWidth + (boxPadding * 2)) / 2) + "px"
+
+		        	});
+
+		        	$("#box").animate({"left":"10px", "top":"10px", "margin-top":"0", "margin-left":"0"});
+
+			    });
+
+			}, 4000);
+
+		}, 300);
+
+	}
+
+	}, (animationTime / 6));
+
+	return false;
+
+}
+
 function resizeHex() {
 
 	var windowHeight = $(window).height();
@@ -46,118 +168,133 @@ function resizeHex() {
 		"overflow":"hidden"
 	});
 
-	while (count <= 7) {
+	if ($("#box .hex.selected").length) {
 
-		switch (count) {
+		$("#box .hex.selected").css({
+			"height":hexHeight + "px", 
+			"width":(hexHeight * 2) + "px",
+			"top":"50%",
+			"margin-top": - (hexHeight / 2) + "px",  
+			"left":"50%", 
+			"margin-left": - hexHeight + "px"
+		});
 
-			case 1:
+	} else {
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",
-					"top": (hexHeight + (hexGap / 2)),  
-					"left":"100%", 
-					"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
-					"margin-top": - (hexHeight / 2) + "px"
+		while (count <= 7) {
 
-				});
+			switch (count) {
 
-			break;
+				case 1:
 
-			case 2:
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",
+						"top": (hexHeight + (hexGap / 2)),  
+						"left":"100%", 
+						"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
+						"margin-top": - (hexHeight / 2) + "px"
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",	
-					"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
-					"left":"100%", 
-					"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
-					"margin-top": - (hexHeight / 2) + "px"
+					});
 
-				});
+				break;
 
-			break;
+				case 2:
 
-			case 3:
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",	
+						"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
+						"left":"100%", 
+						"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
+						"margin-top": - (hexHeight / 2) + "px"
 
-				$("#box .hex" + count).css({
- 					
- 					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",
-					"top":"100%",  
-					"left":"50%", 
-					"margin-left": - hexHeight + "px",
-					"margin-top": - hexHeight + "px"
+					});
 
-				});
+				break;
 
-			break;
+				case 3:
 
-			case 4:
+					$("#box .hex" + count).css({
+	 					
+	 					"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",
+						"top":"100%",  
+						"left":"50%", 
+						"margin-left": - hexHeight + "px",
+						"margin-top": - hexHeight + "px"
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",
-					"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
-					"left":"0", 
-					"margin-left": - (hexOffset - boxPadding) + "px",
-					"margin-top": - (hexHeight / 2) + "px"
+					});
 
-				});
+				break;
 
-			break;
+				case 4:
 
-			case 5:
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",
+						"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
+						"left":"0", 
+						"margin-left": - (hexOffset - boxPadding) + "px",
+						"margin-top": - (hexHeight / 2) + "px"
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",	
-					"top": (hexHeight + (hexGap / 2)),  
-					"left":"0", 
-					"margin-left": - (hexOffset - boxPadding) + "px",
-					"margin-top": - (hexHeight / 2) + "px"
+					});
 
-				});
+				break;
 
-			break;
+				case 5:
 
-			case 6:
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",	
+						"top": (hexHeight + (hexGap / 2)),  
+						"left":"0", 
+						"margin-left": - (hexOffset - boxPadding) + "px",
+						"margin-top": - (hexHeight / 2) + "px"
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",
-					"top":"0",  
-					"left":"50%", 
-					"margin-left": - hexHeight + "px",
-					"margin-top": "0px"
+					});
 
-				});
+				break;
 
-			break;
+				case 6:
 
-			case 7:
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",
+						"top":"0",  
+						"left":"50%", 
+						"margin-left": - hexHeight + "px",
+						"margin-top": "0px"
 
-				$("#box .hex" + count).css({
-					
-					"height":hexHeight + "px", 
-					"width":(hexHeight * 2) + "px",
-					"top":"50%",
-					"margin-top": - (hexHeight / 2) + "px",  
-					"left":"50%", 
-					"margin-left": - hexHeight + "px"
+					});
 
-				});
+				break;
 
-			break;
+				case 7:
+
+					$("#box .hex" + count).css({
+						
+						"height":hexHeight + "px", 
+						"width":(hexHeight * 2) + "px",
+						"top":"50%",
+						"margin-top": - (hexHeight / 2) + "px",  
+						"left":"50%", 
+						"margin-left": - hexHeight + "px"
+
+					});
+
+				break;
+
+			}
+
+			count++;
 
 		}
-
-		count++;
 
 	}
 
@@ -213,14 +350,11 @@ $(function(){
 		"overflow":"hidden"
 	});
 
-	var animationTime = 600;
-	var animationType = "easeOutExpo";
-
 	var newInt = setInterval(function(){
 
 		if (count <= 7) {
 			
-			$("#box").append('<div class="hex hex' + count + '"><div class="in1"><div class="in2"><div class="icon"><i class="fa ' + icons[(count - 1)] + '" style="color:' + colors[(count - 1)] + '"></i></div><div class="text">' + sectionNames[(count - 1)] + '<div class="more">' + moreInfo[(count - 1)] + '</div></div></div></div></div>');
+			$("#box").append('<div class="hex hex' + count + '" data-hexID="' + count + '"><div class="in1"><div class="in2"><div class="icon"><i class="fa ' + icons[(count - 1)] + '" style="color:' + colors[(count - 1)] + '"></i></div><div class="text">' + sectionNames[(count - 1)] + '<div class="more">' + moreInfo[(count - 1)] + '</div></div></div></div></div>');
 
 			$("#box .hex" + count).css({
 
@@ -240,94 +374,120 @@ $(function(){
 				case 1:
 
 					$("#box .hex" + count).animate({
- 						
  						"opacity":"1",
 						"top": (hexHeight + (hexGap / 2)),  
 						"left":"100%", 
 						"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
 						"margin-top": - (hexHeight / 2) + "px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 2:
 
 					$("#box .hex" + count).animate({
- 						
  						"opacity":"1",
 						"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
 						"left":"100%", 
 						"margin-left": - (hexOffset + hexWidth + boxPadding) + "px",
 						"margin-top": - (hexHeight / 2) + "px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 3:
 
 					$("#box .hex" + count).animate({
-	 					
 	 					"opacity":"1",
  						"top":"100%",  
 						"left":"50%", 
 						"margin-left": - hexHeight + "px",
 						"margin-top": - hexHeight + "px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 4:
 
 					$("#box .hex" + count).animate({
- 						
  						"opacity":"1",
  						"top": ((hexHeight * 2) + ((hexGap / 2) * 3)),  
 						"left":"0", 
 						"margin-left": - (hexOffset - boxPadding) + "px",
 						"margin-top": - (hexHeight / 2) + "px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 5:
 
 					$("#box .hex" + count).animate({
- 						
  						"opacity":"1",
  						"top": (hexHeight + (hexGap / 2)),  
 						"left":"0", 
 						"margin-left": - (hexOffset - boxPadding) + "px",
 						"margin-top": - (hexHeight / 2) + "px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 6:
 
 					$("#box .hex" + count).animate({
- 						
  						"opacity":"1",
  						"top":"0",  
 						"left":"50%", 
 						"margin-left": - hexHeight + "px",
 						"margin-top": "0px"
-
-					}, (animationTime), animationType);
+					}, (animationTime), animationType).bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
 				case 7:
 
-					$("#box .hex" + count).animate({
- 						
- 						"opacity":"1"
-
-					}, (animationTime * 4), "easeInOutSine");
+					$("#box .hex" + count).animate({"opacity":"1"}, animationTime, "easeInOutSine").bind('mouseenter mouseleave', function() {
+				        $(this).toggleClass('hover');                                       
+				    }).bind('click', function(){
+						if ($(this).hasClass("hover")) {
+							loadPage($(this).attr("data-hexID"));
+						}
+					});
 
 				break;
 
@@ -336,16 +496,6 @@ $(function(){
 			count++;
 
 		} else {
-
-			$("#box .hex").bind('click', function(){
-				if ($(this).hasClass("hover")) {
-					alert ("hello");
-				}
-			});
-
-			$("#box .hex").bind('mouseenter mouseleave', function() {
-		        $(this).toggleClass('hover');                                       
-		    });
 
 			clearInterval(newInt);
 
